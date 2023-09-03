d0ea51b5cc234620c6dcde6e3d6ca6ebb85ce018
Remik <remigiuszszejna@gmail.com>
2023-09-03 22:08:24 +0200

    Added Spring boot configuration

---
+++ src/main/java/pl/RemigiuszInstalacje/RemigiuszInstalacjeApplication.java
@@ -0,0 +1,13 @@
+package pl.RemigiuszInstalacje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemigiuszInstalacjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemigiuszInstalacjeApplication.class, args);
+	}
+
+}
